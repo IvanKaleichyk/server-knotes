25708d493891d130e655b6ba2ec4b07735a5e67c
ivankl <iwan.kaleiczyk@proexe.eu>
2021-12-15 23:29:38 +0100

    Jwt extensions

---
+++ src/main/kotlin/com/kaleichyk/utils/extension/Jwt.kt
@@ -0,0 +1,20 @@
+package com.kaleichyk.utils.extension
+
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.application.Application
+
+val Application.algorithm: Algorithm get() = Algorithm.HMAC512(environment.config.stringProperty(SECRET_PATH))
+
+val Application.issuer get() = environment.config.stringProperty(ISSUER_PATH)
+
+val Application.subject get() = environment.config.stringProperty(SUBJECT_PATH)
+
+val Application.realm get() = environment.config.stringProperty(REALM_PATH)
+
+val Application.tokenValidTime get() = environment.config.longProperty(EXPIRATION_TIME_PATH)
+
+private const val SECRET_PATH = "jwt.secret"
+private const val ISSUER_PATH = "jwt.issuer"
+private const val SUBJECT_PATH = "jwt.subject"
+private const val REALM_PATH = "jwt.realm"
+private const val EXPIRATION_TIME_PATH = "jwt.expiration_time"
