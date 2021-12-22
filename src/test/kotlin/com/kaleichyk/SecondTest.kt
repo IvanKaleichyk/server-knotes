450e9d9fe389acc3564c0849088cff2f28d27af7
ivankl <iwan.kaleiczyk@proexe.eu>
2021-12-22 23:47:48 +0100

    add tests

---
+++ src/test/kotlin/com/kaleichyk/SecondTest.kt
@@ -0,0 +1,13 @@
+package com.kaleichyk
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SecondTest {
+
+    @Test
+    fun test() {
+        val b = 19
+        assertEquals(b, 19)
+    }
+}
---
+++ src/test/kotlin/com/kaleichyk/third/ThirdTest.kt
@@ -0,0 +1,13 @@
+package com.kaleichyk.third
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ThirdTest {
+
+    @Test
+    fun thirdTest(){
+        val iu = 8
+        assertEquals(iu, 8)
+    }
+}
